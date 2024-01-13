102054f709599d655e43e4fd3b3428d97052eda2
Muhammed Bojang <mahabojang110@gmail.com>
2024-01-13 23:14:27 +0000

    use jonas course to update state and add spread operators

--- src/Service/postApi.jsx
+++ src/Service/postApi.jsx
@@ -3,7 +3,6 @@ import  http  from "../utils/httpCommon"
 export const getAllPost = async () => {
     try {
         const response = await http.get("posts")
-        // console.log(response)
         return (response.data)
     } catch (error) {
         console.log(error)
--- src/features/post/postSlice.jsx
+++ src/features/post/postSlice.jsx
@@ -1,30 +1,30 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// const initialSatate =[
-//     {
-//       "id": 1,
-//       "title": "Lorem Ipsum",
-//       "content": "Lorem ipsum dolor sit amet, aliqua.",
-//       "author": "John Doe"
-//     },
-//     {
-//       "id": 2,
-//       "title": "Sample Title",
-//       "content": "This is some sample content. consequat.",
-//       "author": "Jane Smith"
-//     },
-//     {
-//       "id": 3,
-//       "title": "Random Thoughts",
-//       "content": "Random content for the third entry. pariatur.",
-//       "author": "Bob Johnson"
-//     }
-//   ]\
+const initialSatate =[
+    {
+      "id": 1,
+      "title": "Lorem Ipsum",
+      "content": "Lorem ipsum dolor sit amet, aliqua.",
+      "author": "John Doe"
+    },
+    {
+      "id": 2,
+      "title": "Sample Title",
+      "content": "This is some sample content. consequat.",
+      "author": "Jane Smith"
+    },
+    {
+      "id": 3,
+      "title": "Random Thoughts",
+      "content": "Random content for the third entry. pariatur.",
+      "author": "Bob Johnson"
+    }
+  ]
 
-const initialSatate = {
-  posts: [],
-  status: 'idle',
-  error: null
-}
+// const initialSatate = {
+//   posts: [],
+//   status: 'idle',
+//   error: null
+// }
   
 
 const postsSlice = createSlice({   
@@ -35,7 +35,7 @@ const postsSlice = createSlice({
             console.log("GET ALL POSTS")
         },
         addedPost: (state, action) => {
-          state.push(action.payload)
+          return [ action.payload, ...state]
         },
         deletePost: (state, action) => {
             console.log("DELELT POST")
@@ -46,6 +46,5 @@ const postsSlice = createSlice({
     }
 })
 
-export const selectAllPost = (state) => state.posts
 export const {addedPost} = postsSlice.actions
 export default  postsSlice.reducer
