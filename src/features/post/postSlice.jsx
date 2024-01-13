4baa00d45523db682e90e434b3bcb4c4318cc8c2
Muhammed Bojang <mahabojang110@gmail.com>
2024-01-13 23:14:01 +0000

    ehnahce the state

--- src/features/post/postSlice.jsx
+++ src/features/post/postSlice.jsx
@@ -1,24 +1,30 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-const initialSatate =[
-    {
-      "id": 1,
-      "title": "Lorem Ipsum",
-      "content": "Lorem ipsum dolor sit amet, aliqua.",
-      "author": "John Doe"
-    },
-    {
-      "id": 2,
-      "title": "Sample Title",
-      "content": "This is some sample content. consequat.",
-      "author": "Jane Smith"
-    },
-    {
-      "id": 3,
-      "title": "Random Thoughts",
-      "content": "Random content for the third entry. pariatur.",
-      "author": "Bob Johnson"
-    }
-  ]
+// const initialSatate =[
+//     {
+//       "id": 1,
+//       "title": "Lorem Ipsum",
+//       "content": "Lorem ipsum dolor sit amet, aliqua.",
+//       "author": "John Doe"
+//     },
+//     {
+//       "id": 2,
+//       "title": "Sample Title",
+//       "content": "This is some sample content. consequat.",
+//       "author": "Jane Smith"
+//     },
+//     {
+//       "id": 3,
+//       "title": "Random Thoughts",
+//       "content": "Random content for the third entry. pariatur.",
+//       "author": "Bob Johnson"
+//     }
+//   ]\
+
+const initialSatate = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
   
 
 const postsSlice = createSlice({   
